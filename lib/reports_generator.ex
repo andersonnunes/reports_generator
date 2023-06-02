932a63255c2e6396b37b9aeaeead1f1fff365a01
Anderson Nunes <anderson.nunes1988@gmail.com>
2023-06-01 22:16:47 -0300

    Retornando o maior valor

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -2,10 +2,12 @@ defmodule ReportsGenerator do
   alias ReportsGenerator.Parser
 
   def build(filename) do
-    "reports/#{filename}"
+    filename
     |> Parser.parse_file()
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
+
+  def fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
 
   defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
 
